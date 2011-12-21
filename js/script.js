c4aab5770c329c1d3a68afb5ec8757ccef9947f9
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2011-12-21 20:36:04 +0100

    Minor change

--- js/script.js
+++ js/script.js
@@ -10,13 +10,13 @@ $(document).ready(function () {
   var initialLocation = $(document).attr('location').hash.substring(1) || 'home';
   $('.' + initialLocation).parent().addClass('active');
   $('.location').click(function () { $('.hover').removeClass('hover'); $(this).toggleClass('hover')});
-  $('nav li').click(function () { $('.active').removeClass('active'); $(this).toggleClass('active')});
+  $('nav li a').click(function () { $('.active').removeClass('active'); $(this).parent().addClass('active')});
   sectionSize();
   $('h1').fitText(0.7);
   $('h2').fitText(1.2);
   $('abbr[title], input[title], img[title], a[title], li[title], input:focus').qtip({
      position: {
-       my: 'bottom center',  // Position my top left...
+       my: 'bottom left',  // Position my top left...
        at: 'top center', // at the bottom left of...
      },
      style: {
