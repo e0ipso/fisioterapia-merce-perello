67a9e33ed6c9900066a81a359bc3b5e1023092de
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2011-12-22 10:10:45 +0100

    Parallax for the about section

--- js/script.js
+++ js/script.js
@@ -14,13 +14,18 @@ $(document).ready(function () {
   sectionSize();
   $('h1').fitText(0.7);
   $('h2').fitText(1.2);
-  $('abbr[title], input[title], img[title], a[title], li[title], input:focus').qtip({
+  $('abbr[title], input[title], img[title], a[title], li[title]').qtip({
      position: {
-       my: 'bottom left',  // Position my top left...
+       my: 'bottom center',  // Position my top left...
        at: 'top center', // at the bottom left of...
      },
      style: {
        classes: 'ui-tooltip-cream ui-tooltip-shadow'
      }
   });
+  // Declare parallax on layers
+      jQuery('.parallax-layer').parallax({
+          mouseport: jQuery("#port")
+      });
+  $("section").scrollable({ vertical: true, mousewheel: true });
 });
