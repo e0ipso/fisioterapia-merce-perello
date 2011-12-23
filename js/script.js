d9bdf6980b8ae2e6db6f15ec905f6f348773e187
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2011-12-23 17:13:15 +0100

    Description section and location section

--- js/script.js
+++ js/script.js
@@ -23,9 +23,90 @@ $(document).ready(function () {
        classes: 'ui-tooltip-cream ui-tooltip-shadow'
      }
   });
+  // Colorbox popups
+  $("#show-twitter, #show-facebook").colorbox({
+    iframe:true,
+    width:"600px",
+    height:"600px"
+  });
   // Declare parallax on layers
-      jQuery('.parallax-layer').parallax({
-          mouseport: jQuery("#port")
+  jQuery('.parallax-layer').parallax({
+    mouseport: jQuery("#port")
+  });
+  // Scrolling for the navigation
+  $('a[href*=#]').bind('click',function(event){
+    var $anchor = $(this);
+
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 3000,'easeInOutExpo');
+    event.preventDefault();
+  });
+  
+  // Google Maps code
+  var mallorca = new google.maps.LatLng(39.761723, 3.162496),
+    pointToMoveTo, 
+    first = true,
+    curMarker = new google.maps.Marker({}),
+    $el;
+  
+  var myOptions = {
+    zoom: 10,
+    center: mallorca,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  
+  var map = new google.maps.Map($("#map_canvas")[0], myOptions);
+  
+  $("#locations li").mouseenter(function() {
+  
+    $el = $(this);
+    
+    if (!$el.hasClass("hover")) {
+    
+      $("#locations li").removeClass("hover");
+      $el.addClass("hover");
+      
+      if (!first) { 
+      
+        // Clear current marker
+        curMarker.setMap(); 
+        
+        // Set zoom back to Mallorca level
+        // map.setZoom(10);
+      }
+      
+      // Move (pan) map to new location
+      pointToMoveTo = new google.maps.LatLng($el.attr("data-geo-lat"), $el.attr("data-geo-long"));
+      window.log(pointToMoveTo);  
+      map.panTo(pointToMoveTo);
+      
+      // Add new marker
+      curMarker = new google.maps.Marker({
+        position: pointToMoveTo,
+        map: map,
+  //      icon: "images/marker.png"
       });
-  $("section").scrollable({ vertical: true, mousewheel: true });
+      
+      // On click, zoom map
+      google.maps.event.addListener(curMarker, 'click', function() {
+        map.setZoom(14);
+      });
+      
+      // Fill more info area
+      $("#more-info")
+        .find("h2")
+        .html($el.find("h3").html())
+        .end()
+        .find("p")
+        .html($el.find(".longdesc").html());
+      
+        // No longer the first time through (re: marker clearing)        
+        first = false; 
+    }
+  
+  });
+  
+  $("#locations li:first").trigger("mouseenter");
+  
 });
