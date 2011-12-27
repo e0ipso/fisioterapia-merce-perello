7f6cb3bd1dec6b2d12d243719cd4881f1282331c
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2011-12-27 13:13:43 +0100

    Test values

--- contact.php
+++ contact.php
@@ -1,8 +1,10 @@
 <?php
   $to = "mateu@humanbits.es";
   $subject = "Contact Us";
-  $email = $_REQUEST['email'];
-  $message = $_REQUEST['message'];
+//  $email = $_REQUEST['email'];
+//  $message = $_REQUEST['message'];
+  $email = 'hola@merceperello.es';
+  $message = 'Test';
   $headers = "From: $email";
   $sent = mail($to, $subject, $message, $headers);
   if ($sent) {
